f0082ab0f5008246d08ba11c002ee43b524f219e
jluong102 <jluong102@gmail.com>
2024-05-18 15:07:25 -0400

    Added set of exit codes to verify

--- cmd/silverbullet/monitor.go
+++ cmd/silverbullet/monitor.go
@@ -8,20 +8,32 @@ import (
 type Monitor struct {
 	Interval uint   `json:"interval"`
 	Script   string `json:"script"`
+	Good []int `json:"good"`
 }
 
 func (this Monitor) VerifyMonitor() {
 	this.verifyScript()
+	this.verifyGood()
 }
 
 func (this Monitor) verifyScript() {
+	fmt.Printf("Verifying monitor script %s\n", this.Script)
+
 	info, err := os.Stat(this.Script)
 
 	if err != nil {
 		fmt.Printf("Unable to find file %s: %s\n", this.Script, err)
 		os.Exit(SCRIPT_FILE_ERROR)
 	} else if info.IsDir() {
-		fmt.Printf("Script %s cannont be a directory\n", this.Script)
+		fmt.Printf("Script %s cannot be a directory\n", this.Script)
 		os.Exit(SCRIPT_FILE_ERROR)
+	}
+}
+
+// Make sure that at least one good exit code is provided
+func (this Monitor) verifyGood() {
+	if len(this.Good) < 1 {
+		fmt.Printf("Must provide \"good\" exit codes in \"monitor\"\n")
+		os.Exit(MISSING_SETTING_ERROR)
 	}
 }
